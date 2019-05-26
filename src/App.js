2bb99eb8ddeab88031dc93a7ef6302049650716c
Bharani <barani.potshot@gmail.com>
2019-05-26 18:15:18 +0530

    technology page init

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 import "./css/carousel.css";
 import Menu from "./components/menu/menu";
 import "./style.css";
-import About from "./components/about";
-import Technologies from './components/technologies';
 
 function App() {
   return (
--- src/components/technologies.js
+++ src/components/technologies.js
@@ -1,9 +1,54 @@
 import React from 'react';
 
 class Technologies extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            techs: [
+                {
+                    name: "Angular",
+                    imageRoot: "angular.png",
+                    description: "Angular is a platform and framework for building client applications in HTML and TypeScript"
+                },
+                {
+                    name: "React",
+                    imageRoot: "react.png",
+                    description: "React is a JavaScript library for building user interfaces."
+                }
+            ]
+        };
+    }
     render() {
         return (
-            <div id="wrapper"> Technologies</div>
+            <div id="wrapper">
+                <section className="section lb">
+                    <div className="container">
+                        <div className="section-title text-center">
+                            <h3>Technology</h3>
+                            <p>Not the actual coding and implementation, but the types of technology and the tradeoffs like</p>
+                        </div>
+                        <div className="row text-center">
+                            {
+                                this.state.techs.map((tech, i) => (
+                                    <div key={i} className="col-lg-4 col-md-12">
+                                        <div className="blog-box">
+                                            <div className="post-media">
+                                                <img src={require(`../images/technology/${tech.imageRoot}`)} alt="" className="img-responsive" />
+                                            </div>
+                                            <div className="blog-desc">
+                                                <h4>{tech.name}</h4>
+                                                <p>{tech.description}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </section>
+
+
+            </div>
         );
     }
 }
