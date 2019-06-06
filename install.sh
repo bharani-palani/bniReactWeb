30d91aa67fcb17b8c9c78cf54926272e3aa15ddc
Bharani <barani.potshot@gmail.com>
2019-06-06 19:31:26 +0530

    change 6

--- install.sh
+++ install.sh
@@ -1,15 +1,6 @@
-printf "Installing node modules...\n"
-sleep 7 &
-PID=$!
-i=1
-sp="/-\|"
-printf -n ' '
-while [ -d /proc/$PID ]
-do
-  printf "\b${sp:i++%${#sp}:1}"
-done
+echo -e "Installing node modules...\n"
 npm install
-printf "npm install completed.\n"
-printf "Deploying Bharani React Webiste ...\n"
+echo -e "npm install completed.\n"
+echo -e "Deploying Bharani React Webiste ...\n"
 gcloud app deploy
-printf "Bharani React Webiste Successfully Depolyed ..."
+echo -e "Bharani React Webiste Successfully Depolyed ..."
