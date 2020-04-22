eb8ce769485a9277d7fb492f1f5896e887ba96be
Bharani <btp3@apple.com>
2020-04-22 13:01:07 +0530

    replace session var to resume model

--- services/application/models/resume_model.php
+++ services/application/models/resume_model.php
@@ -33,6 +33,7 @@ class resume_model extends CI_Model
         return get_all_rows($query);
     }
     public function projectExperience() {
+        $this->db->simple_query('SET SESSION group_concat_max_len=15000');
         $this->db->select(
             'c.work_company, 
             concat(a.project_duration_months, " months") as working_duration, 
