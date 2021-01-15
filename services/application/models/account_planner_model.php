25578f717218a2cf3c753fcb62a26ca13d68e5f7
Bharani <btp3@apple.com>
2021-01-15 20:56:13 +0530

    Dr added

--- services/application/models/account_planner_model.php
+++ services/application/models/account_planner_model.php
@@ -44,6 +44,7 @@ class account_planner_model extends CI_Model
 			->from('income_expense as a')
 			->join('income_expense_category as b', 'a.inc_exp_category = b.inc_exp_cat_id', 'left')
 			->where('a.inc_exp_date between '.$year)
+			->where('inc_exp_type', "Dr")
 			->group_by(array("dated", "category"))
 			->order_by("DATE_FORMAT(a.inc_exp_date, '%m-%Y')", "desc");
 		$query = $this->db->get();
